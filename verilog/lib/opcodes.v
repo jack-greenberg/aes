ce763ee8190c9fd316d49961b026d270034c3f7e
Jack Greenberg <j@jackgreenberg.co>
2020-12-02 15:35:41 -0800

    Add regfile

--- verilog/lib/opcodes.v
+++ verilog/lib/opcodes.v
@@ -6,6 +6,8 @@
   `define W_STATE      128 // This is the 128 bit input, intel calls it state
   `define W_ROUNDKEY   128
 
+  `define NUM_ROUNDS 10
+
   // Define inverse enable/disable
   `define INV_EN       `INV_CTRL'b1
   `define INV_DIS      `INV_CTRL'b0
@@ -14,4 +16,20 @@
   `define PASS         `PASSTHRU_CTRL'b1
   `define NOPASS       `PASSTHRU_CTRL'b0
 
-
+  // Register Addresses
+  `define REG_XMM0 `W_KEY'd0
+  `define REG_XMM1 `W_KEY'd1
+  `define REG_XMM2 `W_KEY'd2
+  `define REG_XMM3 `W_KEY'd3
+  `define REG_XMM4 `W_KEY'd4
+  `define REG_XMM5 `W_KEY'd5
+  `define REG_XMM6 `W_KEY'd6
+  `define REG_XMM7 `W_KEY'd7
+  `define REG_XMM8 `W_KEY'd8
+  `define REG_XMM9 `W_KEY'd9
+  `define REG_XMM10 `W_KEY'd10
+  `define REG_XMM11 `W_KEY'd11
+  `define REG_XMM12 `W_KEY'd12
+  `define REG_XMM13 `W_KEY'd13
+  `define REG_XMM14 `W_KEY'd14
+  `define REG_XMM15 `W_KEY'd15
---
+++ verilog/regfile.v
@@ -0,0 +1,40 @@
+`include "lib/opcodes.v"
+`timescale 1ns/1ps
+
+module REGFILE (
+  input clk,
+  input rst,
+  input data_reg,
+  input key_reg,
+  output reg [`W_STATE-1:0] data,
+  output reg [`W_KEY-1:0] key
+);
+  
+  // Storage element
+  // 15 registers
+  reg [`W_KEY-1:0] rf [15:0];
+
+  always @(posedge clk,posedge rst) begin
+    if (rst) begin
+      for(int i = 0; i<32; i=i+1) 
+        rf[i] = 0;
+    end
+    else begin
+      if (`DEBUG_REGFILE) begin
+        /* verilator lint_off STMTDLY */
+        #1 // Delay slightly to correct print timing issue
+        $display("REG FILE: data_reg = %d", data_reg);
+        $display("          key_reg  = %d", key_reg);
+        /* verilator lint_on STMTDLY */
+        $display("$xmm0  = %x $xmm1 = %x $xmm2 = %x $xmm3 = %x",rf[`REG_XMM0], rf[`REG_XMM1],rf[`REG_XMM2],rf[`REG_XMM3]);
+        $display("$xmm4  = %x $xmm5 = %x $xmm6 = %x $xmm7 = %x",rf[`REG_XMM4], rf[`REG_XMM5],rf[`REG_XMM6],rf[`REG_XMM7]);
+        $display("$xmm8  = %x $xmm9 = %x $xmm10 = %x $xmm11 = %x",rf[`REG_XMM8], rf[`REG_XMM9],rf[`REG_XMM10],rf[`REG_XMM11]);
+        $display("$xmm12  = %x $xmm13 = %x $xmm14 = %x $xmm15 = %x",rf[`REG_XMM12], rf[`REG_XMM13],rf[`REG_XMM14],rf[`REG_XMM15]);
+      end
+    end
+  end
+
+  assign  data = rf[data_reg]:0;
+  assign  key = rf[key_reg]:0;
+
+endmodule

a724c8c42c1b62d0577d5e110e53bbdc5f964225
Jackie <jackiezeng01@gmail.com>
2020-12-02 21:56:40 +0000

    implemented addroundkey module

--- verilog/lib/debug.v
+++ verilog/lib/debug.v
@@ -6,6 +6,8 @@
   `define DEBUG_ELU             1'b1
   `define DEBUG_REGFILE         1'b1
   `define DEBUG_SCHEDULER       1'b0
+  `define DEBUG_SUBBYTES        1'b0
+  `define DEBUG_ADDROUNDKEY     1'b0
 
 
 `endif
--- verilog/lib/opcodes.v
+++ verilog/lib/opcodes.v
@@ -1,10 +1,17 @@
 `ifndef OPCODES_V
   `define OPCODES_V 1
 
+  // Width of variables
+  `define W_KEY        128
+  `define W_TEXT       128
+  `define W_ROUNDKEY   128
+
   // Define inverse enable/disable
-  `define INV_EN         `INV_CTRL'b1
-  `define INV_DIS        `INV_CTRL'b0
+  `define INV_EN       `INV_CTRL'b1
+  `define INV_DIS      `INV_CTRL'b0
 
   // Define passthru enable/disable
   `define PASS         `PASSTHRU_CTRL'b1
   `define NOPASS       `PASSTHRU_CTRL'b0
+
+
